5ebc7379db276ce7f666f600fbd513d16f6333d1
Jose <qjoseantonio@gmail.com>
2023-11-14 17:40:52 -0800

    add admin

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class Album(models.Model):
+    title = models.CharField(max_length=250)
+    artist_name = models.CharField(max_length=250)
+    created_at = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def list_albums(request):
+    return render(request, 'albums/index.html')
